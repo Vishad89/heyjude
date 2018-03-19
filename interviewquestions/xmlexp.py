1e48c13a77493cd5a073981208dc881b31c4de28
Vishad Sanghvi <vsanghvi@cray.com>
2018-03-18 20:32:00 -0700

    added parseXML function - test

--- interviewquestions/xmlexp.py
+++ interviewquestions/xmlexp.py
@@ -1,16 +1,46 @@
 
-"""Parse XML file""" 
+"""XML file - Python play""" 
 
 import requests
 from xml.etree import ElementTree
 
 
-def loadfile():
+def loadXML():
 
+	# Get web URL for the newsfeed
 	url='http://www.hindustantimes.com/rss/topnews/rssfeed.xml'
 	resp=requests.get(url)
 
 	with open('newsfeed.xml', 'wb') as f:
 		f.write(resp.content)
 
-loadfile()
+	# Parse the XML file 
+	parseXML(newsfeed.xml)
+
+def parseXML(xmlfile):
+
+	# Create element Tree object for the XML file
+	tree = ElementTree.Parse(xmlfile)
+
+	# Find the root element for the tree
+	root = tree.getroot()
+
+	news = []
+
+	# iterate the newsfeed and add items to the news list
+	for item in root.findall('./channel/item'):
+
+		news.append(item)
+
+	return news
+
+
+
+def main():
+ 
+
+	# Load an XML file from a web URL
+	loadXML()
+
+
+
