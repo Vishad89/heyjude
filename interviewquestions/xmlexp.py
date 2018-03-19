2efe8ede3b18ac05cd4d992e9672629e401c1e91
Vishad Sanghvi <vsanghvi@cray.com>
2018-03-18 18:01:43 -0700

    xml parsing code#1

---
+++ interviewquestions/xmlexp.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+
+
+"""Parse XML file""" 
+
+import requests
+from xml.etree import ElementTree
+
+
+def loadfile():
+
+	url='http://www.hindustantimes.com/rss/topnews/rssfeed.xml'
+	resp=requests.get(url)
+
+	with open('rssfeed.xml', 'r') as f:
+		f.read(resp.content)
