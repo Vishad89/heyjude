3b6ff1d33a9a039f066f853c98338b9a543f090d
vishad89 <vishadsanghvi@gmail.com>
2020-05-18 21:34:40 -0700

    string permuatation

--- Python/interview_questions/string manipulation/string_permuation.py
+++ Python/interview_questions/string manipulation/string_permuation.py
@@ -1,8 +1,17 @@
 
 def permute_string(str):
-    if len(str) == 1:
+    if len(str) <= 1:
         return [str]
    
     perms = permute_string(str[1:])
-    char = str[0] 
-permute_string("abc")
+    print(perms)
+    char=str[0]
+    print(char)
+    result = []
+    for perm in perms:
+        for i in range(len(perm)+1):
+            result.append(perm[:i] + char + perm[i:])
+    return result
+
+
+print(permute_string("abcde"))
