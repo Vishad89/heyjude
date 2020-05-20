8521af2fa7e37978ec8366dc0d1dc5bb58bdf760
vishad89 <vishadsanghvi@gmail.com>
2020-05-20 16:53:23 -0700

    remove after two similar elements

--- Python/interview_questions/two pointers/remove_second_duplicate.py
+++ Python/interview_questions/two pointers/remove_second_duplicate.py
@@ -12,28 +12,33 @@ as new array would become
 arr = [ 0, 0, 1, 1, 2 ]
 
 """
+import sys
 
-def removeDuplictesAfterSecond(arr):
-    i = 0 
-    repeat = 1
-    print(arr)
-    for j in range(1, len(arr)-1):
-        if arr[i] != arr[j]:
-            repeat = 1
-            i += 1
-            arr[i] = arr[j]
-        if arr[i] == arr[j] and repeat >= 2:
-            repeat += 1
-            continue
-        elif arr[i] == arr[j] and repeat < 2:
-            repeat += 1
-            i += 1
-    print(arr)
+def removeDuplicatesSecond(arr):
+    read_pos = 2
+    wrtie_pos = 2
+    #print(arr)
+    while read_pos < len(arr):
+        if arr[read_pos] != arr[wrtie_pos-2]:
+            arr[wrtie_pos] = arr[read_pos]
+            wrtie_pos += 1
+        read_pos += 1
+        print(arr,wrtie_pos,read_pos)
+    arr[wrtie_pos] = '\0'
+    print(arr,wrtie_pos,read_pos)
+    print_array(arr)
 
-
+def print_array(s):
+  i = 0
+  while i < len(s) and str(s[i]) != '\0' :
+    sys.stdout.write(str(s[i]))
+    i += 1
+  print()
+    
 def main():
     a = [0, 0, 1, 1, 1, 2, 2, 3, 4, 5, 5, 5, 5, 6, 7, 8, 8, 8, 9, 10]
     b = [0, 1, 1, 1, 2, 3, 5, 5, 5, 6]
-    return removeDuplictesAfterSecond(a)
+    removeDuplicatesSecond(b)
 
 main()
+
