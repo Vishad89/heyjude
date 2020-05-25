6987e452cc1655285871f69cb6f625e4839b5310
vishad89 <vishadsanghvi@gmail.com>
2020-05-25 01:03:03 -0700

    stack implementation

--- Python/basics/data_structures/stackimplmentation.py
+++ Python/basics/data_structures/stackimplmentation.py
@@ -1,15 +1,32 @@
 # Stack implementation
 
-class stack(object):
+class Slack:
+	def __init__(self):
+		self.items = []
 
-	def __init__(self, data):
-		self.data = data
-		self.next = next
-
-	def push(self,data):
-		self
+	def push(self,item):
+		self.items.append(item)
 
 	def pop(self):
-		self
+		self.items.pop()
 
-	def isEmpty(self)
+	def isEmpty(self):
+		return self.items == []
+
+	def get_stack(self):
+		return self.items
+
+	def peek(self):
+		if not self.isEmpty():
+			return self.items[-1]
+
+	def size(self):
+		return len(self.items)
+
+s = Slack()
+print(s.isEmpty)
+s.push("a")
+s.push("b")
+print(s.isEmpty)
+print(s.peek())
+print(s.size())
